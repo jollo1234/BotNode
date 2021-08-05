bcbae73db5228f3ea463d78aa666cf183d9fb950
jollo1234 <82822859+jollo1234@users.noreply.github.com>
2021-08-05 16:25:06 +0000

    Nothing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client } = require('discord.js');
+
 const client = new Client();
 
 client.on('ready', () => {
@@ -7,14 +8,9 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
 	console.log(`[${message.author.tag}] ${message.content}`);
-	message.react("0️⃣");
 	if(message.author.id != client.user.id) {
 		message.channel.send("a");
 	}
-});
-
-client.on('messageReactionAdd', (messageReaction, user) => {
-	console.log(`[${user.tag}] ${messageReaction.emoji.name}`);
 });
 
 client.login(process.env['TOKEN']);
